b0554a62e8f372819c8a46fe1993ede9a195408d
Ndiasse fall <ndiassefall29086@gmail.com>
2024-04-06 03:50:36 +0000

    reparation du reponsive pour l'adapter sur tout les ecran

--- script.js
+++ script.js
@@ -1,28 +1,25 @@
 const bar = document.querySelector(".bx-menu");
 const cross = document.querySelector(".bx-x");
 const header = document.querySelector(".header");
-const img = document.querySelector(".home-img");
 
 bar.addEventListener("click", function () {
-  setTimeout(() => {
-    cross.style.display = "block";
-  }, 200);
-  header.style.right = "0%";
-  bar.style.display = "none";
-  img.style.marginTop = "300px";
+    setTimeout(() => {
+        cross.style.display = "block";
+    }, 200);
+    header.style.display = "block";
+    bar.style.display = "none";
 });
 
 cross.onclick = () => {
-  cross.style.display = "none";
-  header.style.right = "-100%";
-  bar.style.display = "block";
-  img.style.marginTop = "auto";
+    cross.style.display = "none";
+    header.style.display = "none";
+    bar.style.display = "block";
 };
 
 const sr = ScrollReveal({
-  distance: "40px",
-  duration: 2500,
-  reset: true,
+    distance: "40px",
+    duration: 2500,
+    reset: true,
 });
 
 sr.reveal("header .logo", { delay: 200, origin: "left" });
@@ -33,5 +30,4 @@ sr.reveal(".home-text span", { delay: 600, origin: "top" });
 sr.reveal(".home-text h1", { delay: 680, origin: "left" });
 sr.reveal(".home-text p", { delay: 750, origin: "right" });
 sr.reveal(".main-btn", { delay: 860, origin: "left" });
-// sr.reveal(".share", { delay: 950, origin: "bottom" });
 sr.reveal(".home-img", { delay: 1000, origin: "right" });
